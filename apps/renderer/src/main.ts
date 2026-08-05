acf7e7f93643dacaf1083ef1095e9945703a22bd
wangmingfa <91422761@qq.com>
2026-08-05 22:30:34 +0800

    feat(renderer): 注册 logos + selfhst 图标集，修复新标签页搜索引擎图标不显示 wmf 2 分钟之前

--- apps/renderer/src/main.ts
+++ apps/renderer/src/main.ts
@@ -2,7 +2,9 @@ import './lib/logger'
 import { addCollection } from '@iconify/vue/dist/offline'
 import carbon from '@iconify-json/carbon/icons.json'
 import ic from '@iconify-json/ic/icons.json'
+import logos from '@iconify-json/logos/icons.json'
 import mdi from '@iconify-json/mdi/icons.json'
+import selfhst from '@iconify-json/selfhst/icons.json'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -14,6 +16,8 @@ import { syncThemeToShell } from '@/composables/useTheme'
 addCollection(mdi)
 addCollection(ic)
 addCollection(carbon)
+addCollection(logos)
+addCollection(selfhst)
 
 console.debug('[main] 启动：创建 app 实例并挂载插件')
 const app = createApp(App).use(createPinia()).use(router)
